91f4d41c3e8cb293a856354f1e3a27f537554bc3
iqbal hassan <emiqball111@gmail.com>
2024-01-28 03:20:49 +0500

    create responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 import React from "react";
 
 const App = () => {
-  return <Button colorScheme="blue">Chakra</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //1024px wider
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
