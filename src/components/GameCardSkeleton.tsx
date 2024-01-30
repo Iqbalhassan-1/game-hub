20aed56eb17750a4b7f23255631af34a466e5dc9
iqbal hassan <emiqball111@gmail.com>
2024-01-31 01:44:34 +0500

    added the container for cards

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,14 +2,12 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <>
-      <Card width="260px" borderRadius={10} overflow="hidden">
-        <Skeleton height="200px" />
-        <CardBody>
-          <SkeletonText />
-        </CardBody>
-      </Card>
-    </>
+    <Card>
+      <Skeleton height="200px" />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
   );
 };
 
