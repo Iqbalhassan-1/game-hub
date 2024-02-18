aa73e4dc7a1e3c1793b0d31d072e37ed48770cb8
iqbal hassan <emiqball111@gmail.com>
2024-02-18 15:52:57 +0500

    server to static data for sidebar

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,14 @@
 import useData from "./useData";
+import genres from "../data/genres";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
+// server data
+// const useGenres = () => useData<Genre>("/genres");
 
-const useGenres = () => useData<Genre>("/genres");
+//static
+const useGenres = () => ({ data: genres, isLoading: null, error: null });
 
 export default useGenres;
